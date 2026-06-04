10872c6ee9f77440cff4d3d083daeb21790a5da9
David Vila <152451407+davidvilaa@users.noreply.github.com>
2026-06-04 12:11:01 +0200

    bugfixes

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 
+// @ts-ignore
 import "@/app/globals.css";
 // @ts-ignore
 import "7.css/dist/7.css";
